f1bcf6d787f3e14e465d71d45f43ccd950318c7a
NymeraAnHomie <frostiesnowman439@gmail.com>
2024-12-27 11:43:41 +0800

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -530,30 +530,31 @@ end)
 --// Auto Buy vMerlin
 local LastMerlinBuyTime = 0
 local MerlinPurchaseCount = 0
-RunService.RenderStepped:Connect(function() -- i hate merlin
+RunService.RenderStepped:Connect(function()
     if tick() - LastMerlinBuyTime < 0.2 then return end
     local SafeZoneUI = PlayerGui:FindFirstChild("options") and PlayerGui.options:FindFirstChild("safezone")
-    local RespondMessageOption1 = SafeZoneUI and SafeZoneUI:FindFirstChild("1option") and SafeZoneUI["1option"]:FindFirstChild("button") -- Relic
-    local RespondMessageOption2 = SafeZoneUI and SafeZoneUI:FindFirstChild("2option") and SafeZoneUI["2option"]:FindFirstChild("button") -- Luck
+    local RespondMessageOption1 = SafeZoneUI and SafeZoneUI:FindFirstChild("1option") and SafeZoneUI["1option"]:FindFirstChild("button")
+    local RespondMessageOption2 = SafeZoneUI and SafeZoneUI:FindFirstChild("2option") and SafeZoneUI["2option"]:FindFirstChild("button")    
+    local function buy(option)
+        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
+        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
+    end
     if Options["merlin_auto_buy_luck"].Value then
-        Character.HumanoidRootPart.CFrame = CFrame.new(-928.971863, 225.730988, -995.030945, -0.610376, 0, -0.792112, 0, 1, 0, 0.792112, 0, -0.610376)
+        
     elseif Options["merlin_auto_buy_relic"].Value then
         Character.HumanoidRootPart.CFrame = CFrame.new(-928.971863, 225.730988, -995.030945, -0.610376, 0, -0.792112, 0, 1, 0, 0.792112, 0, -0.610376)
         GuiService.SelectedObject = RespondMessageOption1
-        if GuiService.SelectedObject == RespondMessageOption1 then
-	        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-	        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-        	MerlinPurchaseCount = MerlinPurchaseCount + 1
-        end
-        if not SafeZoneUI then
-            VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
-	        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.E, false, game)
-		end
+        buy(RespondMessageOption1)
+        MerlinPurchaseCount = MerlinPurchaseCount + 1
+    end
+    if not SafeZoneUI then
+        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.E, false, game)
+        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.E, false, game)
     end
     if MerlinPurchaseCount == AutoBuyAmount then
-	    Options["merlin_auto_buy_luck"]:SetValue(false)
-	    Options["merlin_auto_buy_relic"]:SetValue(false)
-	    MerlinPurchaseCount = 0
+        Options["merlin_auto_buy_luck"]:SetValue(false)
+        Options["merlin_auto_buy_relic"]:SetValue(false)
+        MerlinPurchaseCount = 0
     end
 end)
 
